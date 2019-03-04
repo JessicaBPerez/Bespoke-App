3e4c1c5ee7dcc25522a50acc461cc3a666047ae6
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-03-04 12:31:27 -0500

    Added Design Schema to Categories.js

--- models/Categories.js
+++ models/Categories.js
@@ -2,6 +2,14 @@
 const mongoose = require('../db/connection.js')
 const Schema = mongoose.Schema
 
+const DesignSchema = new Schema({
+    collectionPalletImg: String,
+    collectionPalletDescription: String,
+    storeInfo: [],
+    furnitureImg: String,
+    furnitureDescription: String,
+})
+
 const Categories = new Schema({
     categoriesImg: String,
     category: String,
