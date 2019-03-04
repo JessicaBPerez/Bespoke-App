c0ae18bdbf7a7ca3b7792e3de8d74efd553ca38d
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-03-03 19:39:52 -0500

    Added the new.hbs page

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -11,7 +11,8 @@ const categoriesController = {
             //res.send(`Show all categories`)
     },
     new: (req, res) => {
-        res.send(`I'm rending a form to create new Categories`)
+        res.render('new')
+            //res.send(`I'm rending a form to create new Categories`)
     },
     create: (req, res) => {
         res.send(`I'm creating a new category in the database`)
